fd82a5f724e0ca03179f5c81f7795b43fbede7ae
Eman Hayat <emanhayat62@gmail.com>
2022-09-16 07:07:50 -0700

    hello

---
+++ app/src/main/java/com/nooruleman/smd_sp_b/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.nooruleman.smd_sp_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
